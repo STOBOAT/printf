6452172637da115a881a7d6614a9305fc81dd0e4
STOBOAT <stoboat2015@gmail.com>
2023-10-16 13:27:33 -0700

    Prints string

--- 0-string_out.c
+++ 0-string_out.c
@@ -3,37 +3,31 @@
 #include <unistd.h>
 
 _printf(const char *format, ...)
-
-int main(void)
-{
-	_printf("This string is for the popping!");
-	return (0);
-}
-_printf(const char *format, ...)
 {
 	va_list newprintf;
 	char *format;
 	int output;
 
-	va_start(newprintf,format)
-		
+	va_start(newprintf, format)
 		format = (char *)malloc((format * sizeof(char)) + 1)
 
-		for (output = 0; output&& format != '\0'; i++)
+		for (output = 0; output && format != '\0'; i++)
 		{
 			if (*format == '%')
 				format++;
-			switch(*format)
+			switch (*format)
 			{
 				case 'c':
 					{
 						char c = va_arg(newprintf, int);
+
 						write(1, &c, sizeof(char));
 						break;
 					}
 				case 's':
 					{
 						char *s = va_arg(newprintf, *);
+
 						strSize = strlen(s) * sizeof(char);
 
 						write(1, &s, strSize);
@@ -48,7 +42,7 @@ _printf(const char *format, ...)
 		}
 	else
 	{
-		write(1,&format, strln(char));
+		write(1, &format, strln(char));
 	}
 	va_end(format)
 		free(format)
