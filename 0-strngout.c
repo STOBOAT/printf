3ab839d8db892c815e199fd89a8590445e3c7544
STOBOAT <stoboat2015@gmail.com>
2023-10-16 16:28:21 -0700

    edited stringout

---
+++ 0-strngout.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+_printf(const char *format, ...)
+{
+	va_list newprintf;
+	char *strn;
+	int strSize;
+
+	va_start(newprintf, format);
+
+	strn = (char *)malloc((strlen(format) * sizeof(char)) + 1);
+	for (int i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+			i++;
+		switch (format[i])
+		{
+			case 'c':
+				{
+					char c = va_arg(newprintf, int);
+
+					write(1, &c, sizeof(char));
+					break;
+				}
+			case 's':
+				{
+					char *s = va_arg(newprintf, *);
+
+					strSize = strlen(s) * sizeof(char);
+					write(1, s, strSize);
+					break;
+				}
+			case '%':
+				{
+					_putchar('%');
+					break;
+				}
+		}
+		else
+		{
+			write(1, &strn, sizeof(char));
+		}
+		va_end(newprintf);
+		free(strn);
+	}
+}
