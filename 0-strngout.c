98abe6cc2e02fec381014c47ddb7e4338882fc83
donatusbishung <donaldbishung@yahoo.com>
2023-10-17 07:01:38 +0100

    my try on printf

--- 0-strngout.c
+++ 0-strngout.c
@@ -1,50 +1,55 @@
-#include <stdlib.h>
 #include <limits.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
 
-_printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-	va_list newprintf;
-	char *strn;
-	int strSize;
+	va_list args;
 
-	va_start(newprintf, format);
+	va_start(args, format);
 
-	strn = (char *)malloc((strlen(format) * sizeof(char)) + 1);
-	for (int i = 0; format[i] != '\0'; i++)
+	int char_count = 0;
+
+	while (*format)
 	{
-		if (format[i] == '%')
-			i++;
-		switch (format[i])
+		if (*format != '%')
 		{
-			case 'c':
-				{
-					char c = va_arg(newprintf, int);
-
-					write(1, &c, sizeof(char));
-					break;
-				}
-			case 's':
-				{
-					char *s = va_arg(newprintf, *);
-
-					strSize = strlen(s) * sizeof(char);
-					write(1, s, strSize);
-					break;
-				}
-			case '%':
-				{
-					_putchar('%');
-					break;
-				}
+			putchar(*format);
+			char_count++;
 		}
 		else
 		{
-			write(1, &strn, sizeof(char));
+			formt++;
+			if (*format == '\0')
+				break;
+			if (*format == 'c')
+			{
+				int c = va_arg(args, int);
+
+				putchar(c);
+				char_count++;
+			}
+			else if (*format == 's')
+			{
+				const char *str = va_args(args, const char *);
+
+				while (*str)
+				{
+					putchar(*str);
+					str++;
+					char_count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				char_count++;
+			}
 		}
-		va_end(newprintf);
-		free(strn);
+		format++;
 	}
+
+	va_end(args);
+	return (char_count);
 }
